df39441be8aaf7afbb7f77d6ab5c12f1a31be9b6
samiran <119873097+samiranqwerty@users.noreply.github.com>
2023-10-18 00:30:03 +0530

    added write page + completed front end

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import styles from "./loginPage.module.css";
 
 const LoginPage = () => {
-  return <div className={styles.container}>LoginPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={`${styles.socialButton} ${styles.google}`}>
+          Sign-In with Google
+        </div>
+        <div className={`${styles.socialButton} ${styles.github}`}>
+          Sign-In with Github
+        </div>
+        <div className={`${styles.socialButton} ${styles.facebook}`}>
+          Sign-In with Facebook
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
