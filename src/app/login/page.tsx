d2005658dfedc429ff34c876ff554197c72d4fbc
samiran <119873097+samiranqwerty@users.noreply.github.com>
2023-10-17 19:48:48 +0530

    created page for single blog post + working on login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./loginPage.module.css";
+
+const LoginPage = () => {
+  return <div className={styles.container}>LoginPage</div>;
+};
+
+export default LoginPage;
--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./comments.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const Comments = () => {
   const status = "authenticated";
@@ -19,6 +20,29 @@ const Comments = () => {
       ) : (
         <Link href="/login">Login to write Comment</Link>
       )}
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/pp1.png"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>Samiran</span>
+              <span className={styles.date}>17-10-2023</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
+            voluptatum neque quas repellendus, necessitatibus sapiente facere
+            ipsam! Quasi soluta numquam molestiae sapiente placeat culpa
+            necessitatibus nulla alias id, aperiam iure.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
